ca431ec898df64435ffd5c8f66d1b78d43555dee
Tony Istomin <tony.istomin@heattransformers.com>
2024-12-08 22:37:10 +0100

    Solve day 4 part 1

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,38 +1,67 @@
+use advent_of_code::{Addressable, Arithmetical, Coord, Crossword, Vector};
+
 advent_of_code::solution!(4);
 
-#[derive()]
-struct Coord {
-    x: usize,
-    y: usize,
-}
-
-type Vector = Coord;
-
 pub fn part_one(input: &str) -> Option<u32> {
-    let crossword: Vec<Vec<char>> = input
+    let crossword: Crossword = input
         .split('\n')
         .map(|line| line.chars().collect::<Vec<char>>())
         .collect();
-    
+
     let height = crossword.len();
     let width = crossword[0].len();
     let mut count = 0;
-    
+
     for i in 0..height {
         for j in 0..width {
-            if crossword[i][j] != 'X' {
-                continue;
-            }
-            
-            count += count_xmas(&crossword, Coord{ x: i, y: j });
+            count += count_xmas(&crossword, &Coord{ x: i as i32, y: j as i32 });
         }
     }
-    
+
     return Some(count);
 }
 
-fn count_xmas(crossword: &Vec<Vec<char>>, coord: Coord) -> u32 {
+fn count_xmas(crossword: &Crossword, coord: &Coord) -> u32 {
+    let directions = [
+      Vector { x: -1, y: -1 },  
+      Vector { x: -1, y: 0 },  
+      Vector { x: -1, y: 1 },  
+      Vector { x: 0, y: -1 },  
+      Vector { x: 0, y: 1 },  
+      Vector { x: 1, y: -1 },  
+      Vector { x: 1, y: 0 },  
+      Vector { x: 1, y: 1 },  
+    ];
     
+    let result = directions
+        .map(|dir| check_direction(crossword, coord, &dir))
+        .map(|has_xmas| if has_xmas { 1 } else { 0 })
+        .iter().sum();
+
+    return result;
+}
+
+fn check_direction(crossword: &Crossword, coord: &Coord, direction: &Vector) -> bool {
+    if crossword.at(coord) != Some('X') {
+        return false;
+    }
+    
+    let m_coord = coord.add(&direction);
+    if crossword.at(&m_coord) != Some('M') {
+        return false;
+    }
+    
+    let a_coord = m_coord.add(&direction);
+    if crossword.at(&a_coord) != Some('A') {
+        return false;
+    }
+    
+    let s_coord = a_coord.add(&direction);
+    if crossword.at(&s_coord) != Some('S') {
+        return false;
+    }
+    
+    return true;
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,64 @@
 pub mod template;
 
-// Use this file to add helper functions and additional modules.
+pub struct Coord {
+    pub x: i32,
+    pub y: i32,
+}
+
+pub struct Vector {
+    pub x: i32,
+    pub y: i32,
+}
+
+pub type Crossword = Vec<Vec<char>>;
+
+pub trait Addressable {
+    fn at(&self, coord: &Coord) -> Option<char>;
+}
+
+pub trait Bounded {
+    fn width(&self) -> usize;
+    fn height(&self) -> usize;
+    fn is_in_bounds(&self, coord: &Coord) -> bool;
+}
+
+impl Addressable for Crossword {
+    fn at(&self, coord: &Coord) -> Option<char> {
+        if self.is_in_bounds(coord) {
+            return Some(self[coord.x as usize][coord.y as usize]);
+        };
+        
+        return None;
+    }
+}
+
+impl Bounded for Crossword {
+    fn width(&self) -> usize {
+        if self.len() == 0 {
+            return 0;
+        }
+        
+        return self[0].len();
+    }
+
+    fn height(&self) -> usize {
+        return self.len();
+    }
+    
+    fn is_in_bounds(&self, coord: &Coord) -> bool {
+        return coord.x >= 0 
+            && coord.y >= 0
+            && coord.x < (self.height() as i32) 
+            && coord.y < (self.width() as i32);
+    }
+}
+
+pub trait Arithmetical {
+    fn add(&self, vector: &Vector) -> Coord;
+}
+
+impl Arithmetical for Coord {
+    fn add(&self, vector: &Vector) -> Coord {
+        return Coord { x: self.x + vector.x, y: self.y + vector.y };
+    }
+}
