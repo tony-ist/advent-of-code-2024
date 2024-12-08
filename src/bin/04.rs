f376b3334a0bc5bd60bd760983dc9a20c38d90b4
Tony Istomin <tony.istomin@heattransformers.com>
2024-12-08 22:53:23 +0100

    Solve day 4 part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,19 +1,13 @@
-use advent_of_code::{Addressable, Arithmetical, Coord, Crossword, Vector};
+use advent_of_code::{Addressable, Arithmetical, Bounded, Coord, Crossword, Vector};
 
 advent_of_code::solution!(4);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let crossword: Crossword = input
-        .split('\n')
-        .map(|line| line.chars().collect::<Vec<char>>())
-        .collect();
-
-    let height = crossword.len();
-    let width = crossword[0].len();
+    let crossword: Crossword = crossword_from_str(&input);
     let mut count = 0;
 
-    for i in 0..height {
-        for j in 0..width {
+    for i in 0..crossword.height() {
+        for j in 0..crossword.width() {
             count += count_xmas(&crossword, &Coord{ x: i as i32, y: j as i32 });
         }
     }
@@ -65,7 +59,47 @@ fn check_direction(crossword: &Crossword, coord: &Coord, direction: &Vector) -> 
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let crossword: Crossword = crossword_from_str(&input);
+    let mut count = 0;
+
+    for i in 0..crossword.height() {
+        for j in 0..crossword.width() {
+            if (has_cross_mas(&crossword, &Coord{ x: i as i32, y: j as i32 })) {
+                count += 1;
+            }
+        }
+    }
+
+    return Some(count);
+}
+
+fn has_cross_mas(crossword: &Crossword, coord: &Coord) -> bool {
+    if crossword.at(&coord) != Some('A') {
+        return false;
+    }
+    
+    let top_left = coord.add(&Vector { x: -1, y: -1 });
+    let top_right = coord.add(&Vector { x: -1, y: 1 });
+    let bottom_left = coord.add(&Vector { x: 1, y: -1 });
+    let bottom_right = coord.add(&Vector { x: 1, y: 1 });
+    
+    if is_m_and_s(crossword.at(&top_left), crossword.at(&bottom_right)) 
+        && is_m_and_s(crossword.at(&top_right), crossword.at(&bottom_left)) {
+        return true;
+    }
+    
+    return false;
+}
+
+fn is_m_and_s(char1: Option<char>, char2: Option<char>) -> bool {
+    return char1 == Some('M') && char2 == Some('S') || char1 == Some('S') && char2 == Some('M');
+}
+
+fn crossword_from_str(input: &str) -> Crossword {
+    return input
+        .split('\n')
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect();
 }
 
 #[cfg(test)]
@@ -81,6 +115,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9));
     }
 }
